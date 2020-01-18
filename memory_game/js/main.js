3f97249bbf071f025e30f66b469dc7c782697655
Afnan AL-mutairi <afnanxmt@gmail.com>
2020-01-18 23:35:03 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,20 @@
-console.log("Up and running!");
 
-let cardOne= "Queen";
-let cardTwo= "King";
-let cardThree= "Queen";
-let cardFour= "King";
 
-console.log("User flipped " + cardOne);
+const cards=["queen","queen","king","king"];
+let cardsInPlay=[]; 
+let cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("user flipped queen"); 
+let CardTwo = cards[2];
+cardsInPlay.push('CardTwo');
+console.log("user flipped King"); 
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	alert ("you have found a match ! ");
+	else
+		alert ("Sorry try agine ! ");
+}
+
+
